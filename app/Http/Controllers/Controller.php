a50658937845ed70faf51f10bc13310e437cdb60
BaekJinYoung <time121412@gmail.com>
2024-11-05 14:56:07 +0900

    feat: 에디터 파일 추가

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 abstract class Controller
 {
-    //
+    protected $model;
+
+    public function __construct($model)
+    {
+        $this->model = $model;
+    }
+
+    protected function getName($view) // 이동할 페이지 이름
+    {
+        return 'editor.' . strtolower(class_basename($this->model)) . ucfirst($view);
+    }
+
+    public function index(Request $request)
+    {
+
+        return view($this->getName('index'));
+    }
+
+    public function create(Request $request)
+    {
+
+        return view($this->getName('create'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,22 @@
 <?php
 
+use App\Http\Controllers\CkEditorController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::prefix('editor')->group(function () { // 관리자 페이지
+    $crudRoutes = [
+        'ckEditor' => CkEditorController::class,
+    ];
+
+    foreach ($crudRoutes as $prefix => $controller) {
+        Route::prefix($prefix)->controller($controller)->group(function () use ($prefix) {
+            Route::get('/', 'index')->name("editor.{$prefix}Index");
+            Route::get('/create', 'create')->name("editor.{$prefix}Create");
+            Route::post('/store', 'store')->name("editor.{$prefix}Store");
+            Route::get('/{id}/edit', 'edit')->name("editor.{$prefix}Edit");
+            Route::patch('/{id}', 'update')->name("editor.{$prefix}Update");
+            Route::delete('/{id}', 'destroy')->name("editor.{$prefix}Destroy");
+        });
+    }
 });
