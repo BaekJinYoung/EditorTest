f9966b82114c163892ee6dca682476cfc1392a99
BaekJinYoung <time121412@gmail.com>
2024-11-06 09:54:59 +0900

    feat: 제목 포함 저장

--- resources/views/editor/ckeditorCreate.blade.php
+++ resources/views/editor/ckeditorCreate.blade.php
@@ -14,6 +14,14 @@
 <body>
 <form action="{{ route('editor.ckEditorStore') }}" method="POST">
     @csrf
+    <div class="form-item row-group">
+        <p class="item-default">
+            제목
+            <span class="red">*</span>
+        </p>
+        <textarea rows="2" name="title" id="title" placeholder="제목을 입력하세요."
+                  required>{{ old('title') }}</textarea>
+    </div>
     <textarea name="content" id="editor"></textarea>
     <button type="submit">Submit</button>
 </form>
